3d325317849b0ceacd782ad82e8455bfe71a94fa
enrico brasil <enrico.brasil@prit.app>
2025-04-01 14:34:42 -0300

    fit:homeScreen

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
-
-function App() {
-  const [count, setCount] = useState(0)
+import Home from "./screens/Home"
+export default function App() {
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <Home/>
     </>
   )
 }
 
-export default App
