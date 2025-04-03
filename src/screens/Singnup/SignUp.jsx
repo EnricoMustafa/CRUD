1fd8892c4b86636b6752febf1b1cac7c5325df57
enrico brasil <enrico.brasil@prit.app>
2025-04-03 12:12:27 -0300

    routes home

--- src/screens/Singnup/SignUp.jsx
+++ src/screens/Singnup/SignUp.jsx
@@ -3,7 +3,7 @@ export default function SignUp() {
     return (
       <>
           <div className="h-screen w-full bg-slate-500 flex items-center justify-center">
-              <form className="flex flex-col h-96 w-96 bg-white rounded-lg p-6 shadow-lg">
+              <form className="flex flex-col h-100 w-96 bg-white rounded-lg p-6 shadow-lg">
                   <h1 className="text-center text-3xl">Sign Up</h1>
                   <div className="flex  flex-col items-start mx-auto mt-3">
                       <label className="font-semibold">Email</label>
@@ -19,6 +19,7 @@ export default function SignUp() {
                   </div>
                   <div className="flex flex-col items-center justify-center mt-5">
                   <button className="bg-gray-900 text-white rounded-lg p-2 w-44 hover:bg-white hover:shadow-xl hover:text-gray-900">Sign up</button> 
+                  <p className="text-slate-400 mt-2 flex justify-center text-sm" href=""><a className="underline px-1 cursor-pointer hover:text-gray-800" href="/">Sign in</a></p>
                   </div>
               </form>
           </div>
